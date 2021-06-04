7c0077aa861fae06160d6f6097905c6f5a12424d
Gengur123 <meleearthur@gmail.com>
2021-06-04 09:28:59 -0700

    Added 2 more test radio questions and added what page you are on

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,11 @@ $(document).ready(function() {
   $("form#Q1").submit(function(event) {
     event.preventDefault();
   $("#Q1").hide();
-
+  $("#Q2").show();
+  });
+  $("form#Q2").submit(function(event) {
+    event.preventDefault();
+  $("#Q2").hide();
+  $("#Q3").show();
   });
 });
